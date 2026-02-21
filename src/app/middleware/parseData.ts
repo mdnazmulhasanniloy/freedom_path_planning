6ddf8890318a8a7bdbda29030171d14a506426a6
MD Nazmul  Hasan <nazmul@sparktechagency.com>
2026-02-21 10:01:01 +0600

    some changes

--- src/app/middleware/parseData.ts
+++ src/app/middleware/parseData.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import catchAsync from '../utils/catchAsync';
 const parseData = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    console.log('-------------------------->>>', req.body);
     if (req?.body?.data) {
       req.body = JSON.parse(req.body.data);
     }
