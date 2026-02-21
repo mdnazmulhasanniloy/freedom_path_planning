c9df4987d5eb163a4e5a799f9b5b8f4ed90f0a92
MD Nazmul  Hasan <nazmul@sparktechagency.com>
2026-02-21 10:20:51 +0600

    fixed error

--- src/app/middleware/parseData.ts
+++ src/app/middleware/parseData.ts
@@ -2,12 +2,28 @@ import { Request, Response, NextFunction } from 'express';
 import catchAsync from '../utils/catchAsync';
 const parseData = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    console.log('-------------------------->>>', req.body);
-    if (req?.body?.data) {
-      req.body = JSON.parse(req.body.data);
-    }
 
-    next();
-  });
-};
+   
+        if (req.body?.data && typeof req.body.data === 'string') {
+          const parsed = JSON.parse(req.body.data);
+
+          req.body = {
+            ...parsed,
+            ...req.body,  
+          };
+
+          delete req.body.data;
+        }
+
+        next();
+      },
+    );
+  }
+
+    // if (req?.body?.data) {
+    //   req.body = JSON.parse(req.body.data);
+    // }
+
+    // next();
+  
 export default parseData;
